fa92eb185302768c7b6fc2a9f9e3c3a321cfe7a8
scarletsky <scarletsky1025@gmail.com>
2021-06-10 21:17:22 +0800

    chore: update .doom.d

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,13 +1,19 @@
-(define-key key-translation-map (kbd "SPC f f") (kbd "C-x C-f"))
 (define-key key-translation-map (kbd "SPC m c c") (kbd "C-c C-c"))
 (define-key key-translation-map (kbd "SPC m c k") (kbd "C-c C-k"))
 
-(map! :en "C-h"   #'evil-window-left
-      :en "C-j"   #'evil-window-down
-      :en "C-k"   #'evil-window-up
-      :en "C-l"   #'evil-window-right
-      :en "M-."   #'tide-jump-to-definition
-      :en "M-,"   #'tide-jump-back)
+(setq doom-font (font-spec :family "Monaco" :size 14 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Monaco") ; inherits `doom-font''s :size
+      doom-unicode-font (font-spec :family "Monaco" :size 14)
+      doom-big-font (font-spec :family "Monaco" :size 22))
+
+(map! :en "C-h"       #'evil-window-left
+      :en "C-j"       #'evil-window-down
+      :en "C-k"       #'evil-window-up
+      :en "C-l"       #'evil-window-right
+      :en "M-."       #'tide-jump-to-definition
+      :en "M-,"       #'tide-jump-back
+      :en "C-["       #'evil-force-normal-state
+      :en "SPC f f"   #'counsel-find-file)
 
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 ; ;; (add-to-list 'default-frame-alist '(ns-appearance . dark))
